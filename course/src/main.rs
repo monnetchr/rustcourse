800e0d6c53e838b0152bb1cd7a94d48f652b93da
Christophe Monnet <christophe.monnet@sbb.ch>
2026-02-08 13:08:54 +0100

    interior mutability

--- course/src/main.rs
+++ course/src/main.rs
@@ -1,7 +1,7 @@
 use basics;
 use myutils::stuff;
 use myutils::testing;
-use guessgame;
+//use guessgame
 
 fn main() {
     basics::basics();
@@ -10,6 +10,7 @@ fn main() {
     stuff::lifetimes();
     testing::add(2, 2);
     println!();
-    stuff::error_handling();
-    guessgame::guess_game();
+    //stuff::error_handling();
+    //guessgame::guess_game();
+    intmut::intmut();
 }
